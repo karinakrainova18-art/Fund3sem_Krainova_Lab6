50f465536d92bfd1d8a5cbfffc26f1b38f7e7c4a
karinakrainova18-art <karinakrainova18@gmail.com>
2025-11-28 17:02:59 +0300

    Commit: Lab6 files

---
+++ brackets_main.c
@@ -0,0 +1,21 @@
+#include "brackets.h"
+#include <stdio.h>
+
+int main() {
+    printf("Enter string to check brackets: ");
+    char input[256];
+    
+    if (fgets(input, sizeof(input), stdin)) {
+        size_t len = strlen(input);
+        if (len > 0 && input[len-1] == '\n') 
+            input[len-1] = '\0';
+            
+        int result = check_balanced(input);
+        printf("String: %s\nResult: %s\n", input, get_result_msg(result));
+    } else {
+        printf("Error reading input\n");
+        return 1;
+    }
+    
+    return 0;
+}
